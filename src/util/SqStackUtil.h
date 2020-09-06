10d13fd7575456e21acf7864ec7b2fa15e556d37
modi1492 <modi1492@163.com>
2020-09-06 19:03:14 +0800

    Finish SqStack Line Edit Test

--- src/util/SqStackUtil.h
+++ src/util/SqStackUtil.h
@@ -5,8 +5,12 @@
 #ifndef DATASTRUCTURECLIONCMAKEUNITTEST_SQSTACKUTIL_H
 #define DATASTRUCTURECLIONCMAKEUNITTEST_SQSTACKUTIL_H
 
-#include <stdio.h>
+#include <cstdio>
 #include "basic/Stack/SqStack/SqStack.h"        //**▲03 栈和队列**//
+#ifdef  EOF
+#undef  EOF
+#define EOF '\0'
+#endif
 
 /*
  * ████████ 算法3.1 ████████
@@ -15,6 +19,17 @@
  */
 void conversion(int i);
 
+/*
+ * ████████ 算法3.2 ████████
+ *
+ * 行编辑程序，模拟编辑文本时的退格与清空行的操作。
+ *
+ *【注】
+ * 教材使用的是控制台输入，这里为了便于测试，直接改为从形参接收参数
+ */
+void LineEdit(const char buffer[]);
 
+// 测试函数，打印元素
+void Print(SElemType e);
 
 #endif //DATASTRUCTURECLIONCMAKEUNITTEST_SQSTACKUTIL_H
--- test/Stack/SqStackTest.cpp
+++ test/Stack/SqStackTest.cpp
@@ -109,3 +109,17 @@ TEST(SqStack, SqStackConversion) {
 
 }
 
+TEST(SqStack, SqStackLineEdit) {
+    char* buf = "whli##ilr#e(s#*s)\noutcha@    putchar(*s=#++);";   //需要录入的内容
+
+    printf("作为示范，用户输入的文本内容为：\n");
+    printf("%s\n\n", buf);
+
+    printf("进入行编辑程序...\n\n");
+
+    printf("特殊符号：'#' 代表删除上一元素　'@' 代表删除当前输入行\n");
+    printf("         '\\n'代表此行输入无误　'\\0'代表输入结束\n");
+    printf("最终存储的内容为：\n");
+    LineEdit(buf);
+}
+
