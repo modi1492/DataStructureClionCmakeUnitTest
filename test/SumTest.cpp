484cddae3c0c9404481374625709d8cdc8c4c92b
modi1492 <modi1492@163.com>
2020-09-07 23:09:34 +0800

    format code

--- test/SumTest.cpp
+++ test/SumTest.cpp
@@ -10,25 +10,28 @@ using namespace std;
 
 TEST(Sum, SumT) {
     int n = 10;
-    int* A = ( int* ) malloc ( n * sizeof ( int ) ); //ToDo: check NULL != A
-    srand ( ( unsigned int ) time ( NULL ) );
+    int *A = (int *) malloc(n * sizeof(int)); //ToDo: check NULL != A
+    srand((unsigned int) time(NULL));
 // 顺序
-    for ( int i = 0; i < n; i++ ) A[i] = i; print ( A, n );
-    printf ( "SumI  = %d\n", sumI ( A, n ) );
-    printf ( "SumR1 = %d\n", sum ( A, n ) );
-    printf ( "SumR2 = %d\n\n", sum ( A, 0, n ) );
+    for (int i = 0; i < n; i++) A[i] = i;
+    print(A, n);
+    printf("SumI  = %d\n", sumI(A, n));
+    printf("SumR1 = %d\n", sum(A, n));
+    printf("SumR2 = %d\n\n", sum(A, 0, n));
 // 随机置乱
-    shuffle ( A, n ); print ( A, n );
-    printf ( "SumI  = %d\n", sumI ( A, n ) );
-    printf ( "SumR1 = %d\n", sum ( A, n ) );
-    printf ( "SumR2 = %d\n\n", sum ( A, 0, n ) );
+    shuffle(A, n);
+    print(A, n);
+    printf("SumI  = %d\n", sumI(A, n));
+    printf("SumR1 = %d\n", sum(A, n));
+    printf("SumR2 = %d\n\n", sum(A, 0, n));
 // 前后颠倒
-    reverse ( A, n ); print ( A, n );
-    printf ( "SumI  = %d\n", sumI ( A, n ) );
-    printf ( "SumR1 = %d\n", sum ( A, n ) );
-    printf ( "SumR2 = %d\n\n", sum ( A, 0, n ) );
+    reverse(A, n);
+    print(A, n);
+    printf("SumI  = %d\n", sumI(A, n));
+    printf("SumR1 = %d\n", sum(A, n));
+    printf("SumR2 = %d\n\n", sum(A, 0, n));
 //
-    free ( A );
-    printf ( "Test done.\a\n" );
+    free(A);
+    printf("Test done.\a\n");
 }
 
